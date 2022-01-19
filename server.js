eceb8d2498876eccc03cdd8109469cc255d045a9
Diyar <diyar@Diyars-MacBook-Pro-M1.local>
2022-01-19 17:05:22 +0300

    adding files

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
+import questionRoute from "./routes/questions.js";
 import Cors from "cors";
-import dotenv from "dotenv";
-import User from "./models/user.js";
-dotenv.config();
-
-import questionRoute from "./routes/question.js";
-import userRoute from "./routes/user.js";
-import resultRoute from "./routes/result.js";
 const app = express();
 const port = process.env.PORT || 8000;
+
 //connect MongoDB
-// process.env.TOKEN_SECRET
 const connections_url = `mongodb+srv://diyarsali:diyarsali123@cluster0.dvfal.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 mongoose.connect(connections_url, {
   useNewUrlParser: true,
@@ -25,19 +19,11 @@ var db = mongoose.connection;
 app.use(express.json());
 app.use(Cors());
 
-// app.use("/", (req, res) => {
-//   res.send("hellow hellow worlds");
-// });
 app.get("/", (req, res) => {
-  User.find((err, data) => {
-    if (err) res.status(500).send(err);
-    else res.status(200).send(data);
-  });
+  res.send("hellow world");
 });
-
 app.use("/question", questionRoute);
-app.use("/users", userRoute);
-app.use("/result", resultRoute);
 
+//Bind connection to error event (to get notification of connection errors)
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
-app.listen(port, () => console.log(`server is running on port ${port}`));
+app.listen(port, () => console.log(`server is running on port${port}`));
