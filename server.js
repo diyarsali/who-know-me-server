f469c11e1439b8d00e6a23aa8998ec60bc14cb75
Diyar <diyar@Diyars-MacBook-Pro-M1.local>
2022-01-22 15:47:27 +0300

    testing corss

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 //connect MongoDB
 
-const connections_url = `mongodb+srv://diyarsali:${process.env.DBSECRET}@cluster0.dvfal.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const connections_url = `mongodb+srv://diyarsali:diyarsali123@cluster0.dvfal.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 mongoose.connect(connections_url, {
   useNewUrlParser: true,
 
