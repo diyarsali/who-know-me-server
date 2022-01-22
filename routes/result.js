00e8ed4342bfc61aedf494118cc95fd9f4edfe17
Diyar <diyar@Diyars-MacBook-Pro-M1.local>
2022-01-22 23:45:41 +0300

    “final”

--- routes/result.js
+++ routes/result.js
@@ -10,17 +10,21 @@ router.post("/add", async (req, res) => {
   let recieverID = req.body.recieverID;
   let answearUsername = req.body.answearUsername;
   let RightAnswer = req.body.RightAnswer;
+  // console.log(RightAnswer);
 
-  const username = "diyar";
-  Result.findOneAndUpdate(
-    { user: username, answearUsername: answearUsername },
-    { $push: { rightAnswers: RightAnswer } },
-    { upsert: true },
-    function (err, doc) {
-      if (err) return console.log(err);
-      return console.log("inser successfully");
-    }
-  );
+  if (recieverID.match(/^[0-9a-fA-F]{24}$/)) {
+    // const user = await User.find({ _id: recieverID });
+    const username = "diyar";
+    Result.findOneAndUpdate(
+      { user: username, answearUsername: answearUsername },
+      { $push: { rightAnswers: RightAnswer } },
+      { upsert: true },
+      function (err, doc) {
+        if (err) return console.log(err);
+        return console.log("inser successfully");
+      }
+    );
+  }
 });
 
 // to ensure user is answering to another user
