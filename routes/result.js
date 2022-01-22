3b8b70983e47c382396bf9b5c8ea0985d5d957f7
Diyar <diyar@Diyars-MacBook-Pro-M1.local>
2022-01-22 22:34:52 +0300

    im done

--- routes/result.js
+++ routes/result.js
@@ -4,67 +4,59 @@ import Result from "../models/result.js";
 const router = express.Router();
 
 import User from "../models/user.js";
+
 // result saving
-router.post("/add", (req, res) => {
+router.post("/add", async (req, res) => {
   let recieverID = req.body.recieverID;
   let answearUsername = req.body.answearUsername;
   let RightAnswer = req.body.RightAnswer;
   // console.log(RightAnswer);
 
   if (recieverID.match(/^[0-9a-fA-F]{24}$/)) {
-    User.findById(recieverID, function (err, user) {
-      if (err) {
-        console.log(err);
-      } else {
-        // console.log('Result : ', user.username);
+    const user = await User.find({ _id: recieverID });
+    const username = user[0].username;
+    // console.log(username);
 
-        Result.find(
-          {
-            user: user.username,
-            answearUsername: answearUsername,
-          },
-          (err, doc) => {
-            // console.log(doc);
-            if (doc.length == 0) {
-              Result.create(
-                {
-                  user: user.username,
-                  answearUsername: answearUsername,
-                  rightAnswers: RightAnswer,
-                },
-                (err) => {
-                  if (err) {
-                    console.log("Error saving Result into mongo");
-                    return;
-                  }
-                  console.log("Result inserted succussfully!");
-                }
-              );
-            } else {
-              Result.updateOne(
-                {
-                  user: user.username,
-                  answearUsername: answearUsername,
-                },
-                { $push: { rightAnswers: RightAnswer } },
-                (err, data) => {
-                  if (err) {
-                    console.log(err);
-                    return;
-                  }
-                  console.log("updated result");
-                  return;
-                }
-              );
-            }
-          }
-        );
-      }
+    const result = await Result.find({
+      user: username,
+      answearUsername: answearUsername,
     });
+    if (result.length === 0) {
+      Result.create(
+        {
+          user: username,
+          answearUsername: answearUsername,
+          rightAnswers: RightAnswer,
+        },
+        (err) => {
+          if (err) {
+            console.log("Error saving Result into mongo");
+            return;
+          }
+          console.log("Result created succussfully!");
+        }
+      );
+    } else {
+      Result.updateOne(
+        {
+          user: username,
+          answearUsername: answearUsername,
+        },
+        { $push: { rightAnswers: RightAnswer } },
+        (err, data) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          console.log("updated result");
+          return;
+        }
+      );
+    }
   }
 });
 
-// to endure user is answering to another user
+// to ensure user is answering to another user
 router.post("/getResult", (req, res) => {
   let recieverID = req.body.recieverID;
   let answearUsername = req.body.answearUsername;
@@ -87,11 +79,11 @@ router.post("/getResult", (req, res) => {
   }
 });
 
-router.post("/getResultToResult", (req, res) => {
-  Result.find({ user: req.body.username }, (err, data) => {
-    res.send(data);
-    console.log(data);
-  });
-});
+// router.post("/getResultToResult", (req, res) => {
+//   Result.find({ user: req.body.username }, (err, data) => {
+//     res.send(data);
+//     console.log(data);
+//   });
+// });
 
 export default router;
